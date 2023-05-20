a4b91afa8ee93553e94bfde77978b5bdcf95a5ac
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-05-20 16:59:02 +1000

    basic routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,64 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  const data = await Tag.findAll().catch((err) => {
+    res.json(err);
+  });
+  res.json(data);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const data = await ProductTag.findByPk(req.params.id).catch((err) => {
+    res.json(err);
+  });
+  res.json(data);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const data = await ProductTag.create(req.body);
+    // 200 status code means the request is successful
+    res.status(200).json(data);
+  } catch (err) {
+    // 400 status code means the server could not understand the request
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const data = await ProductTag.update(req.body);
+    {
+
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    },
+    // 200 status code means the request is successful
+    res.status(200).json(userData);
+  }; catch (err) {
+    // 400 status code means the server could not understand the request
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-});
+  const data = await ProductTag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  return res.json(data);
+}); 
 
 module.exports = router;
